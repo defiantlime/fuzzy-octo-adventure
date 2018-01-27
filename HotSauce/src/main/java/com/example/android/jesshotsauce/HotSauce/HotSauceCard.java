81960f7ad719ade9ac3648ef84755182c4cab3c9
defiantlime <35365398+defiantlime@users.noreply.github.com>
2018-01-26 18:11:26 -0600

    Business card project.

---
+++ HotSauce/src/main/java/com/example/android/jesshotsauce/HotSauce/HotSauceCard.java
@@ -0,0 +1,13 @@
+package com.example.android.jesshotsauce.HotSauce;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HotSauceCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hot_sauce_card);
+    }
+}
